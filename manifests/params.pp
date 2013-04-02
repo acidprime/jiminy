924df1ee619933509ccaa5e02952afa15349ddd0
Zack Smith <acidprime@wallcity.org>
2013-04-02 07:53:24 +0000

    Cleanup Params.pp
    Prior to this commit I had made this a paramitized class and I don't know why
    Started adding some support for opensource.

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,13 +1,18 @@
-class jiminy::params (
-  $mc_agent_name     = "${module_name}.rb",
-  $mc_agent_ddl_name = "${module_name}.ddl",
-  $mc_app_name       = "${module_name}.rb",
-  $git_server        = 'classroom.puppetlabs.vm',
-  $repo_path         = '/var/repos',
-  $mc_service_name = 'pe-mcollective',
-  $mc_agent_path   = '/opt/puppet/libexec/mcollective/mcollective/agent',
-  $mc_application_path = '/opt/puppet/libexec/mcollective/mcollective/application'
-)
+class jiminy::params
 {
+  if $::is_pe == 'true' {
+    $plugins_dir = '/opt/puppet/libexec/mcollective/mcollective'
+  } else {
+    $plugins_dir = '/usr/libexec/mcollective/mcollective'
+  }
+
+  $mc_agent_name       = "${module_name}.rb"
+  $mc_agent_ddl_name   = "${module_name}.ddl"
+  $mc_app_name         = "${module_name}.rb"
+  $git_server          = 'classroom.puppetlabs.vm'
+  $repo_path           = '/var/repos'
+  $mc_service_name     = 'pe-mcollective'
+  $mc_agent_path       = "${plugins_dir}/agent"
+  $mc_application_path = "${plugins_dir}/application"
 
 }
